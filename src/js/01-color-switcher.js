92f02c89870942484ae9cf71aecd56e973748e02
AntKhom <antonhomenko1988@gmail.com>
2023-08-29 11:57:10 -0400

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalId = null;
+// console.log(startBtn, '...', stopBtn);
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const startChangeColorHandler = () => {
+        intervalId = setInterval(() => {
+            const currentColor = getRandomHexColor();
+            bodyEl.style.backgroundColor = currentColor;
+        }, 1000);
+    // console.log(intervalId);
+
+}
+
+const stopChangeColorHandler = () => {
+    clearInterval(intervalId);
+    // console.log(intervalId);
+}
+
+startBtn.addEventListener('click', startChangeColorHandler);
+stopBtn.addEventListener('click', stopChangeColorHandler);
