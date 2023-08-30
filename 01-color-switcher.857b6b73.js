32c32e74c9894666b77a12f5375e72caaa45b79c
AntKhom <129000861+AntKhom@users.noreply.github.com>
2023-08-30 13:42:08 +0000

    Deploying to gh-pages from @ AntKhom/goit-js-hw-09@cb4bed6d989bc8f29f8a3d041ad67395c03ffbc6 🚀

--- 01-color-switcher.857b6b73.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),o=null;e.addEventListener("click",(function(){o=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));t.style.backgroundColor=e}),1e3)})),n.addEventListener("click",(function(){clearInterval(o)}))}();
-//# sourceMappingURL=01-color-switcher.857b6b73.js.map
