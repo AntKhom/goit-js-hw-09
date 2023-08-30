ad9adb155b079ee04bc7036f189a61a644717ba8
AntKhom <129000861+AntKhom@users.noreply.github.com>
2023-08-30 16:44:00 +0000

    Deploying to gh-pages from @ AntKhom/goit-js-hw-09@b7f7884c267432c3ab3c577bb3ad6d55562a698b 🚀

---
+++ 03-promises.b4492b4f.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(e=>{e.preventDefault(),console.log(e),function(e,t){const o=new Promise(((e,o)=>{const n=Math.random()>.3;setTimeout((()=>{n?e("ok"):o("no")}),t)}));o.then((e=>{alert(e)})),o.cutch((e=>{alert(e)}))}(0,1)}));
+//# sourceMappingURL=03-promises.b4492b4f.js.map
---
+++ 03-promises.f8827801.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(n){var t,e;n.preventDefault(),console.log(n),t=1,(e=new Promise((function(n,e){var o=Math.random()>.3;setTimeout((function(){o?n("ok"):e("no")}),t)}))).then((function(n){alert(n)})),e.cutch((function(n){alert(n)}))}));
+//# sourceMappingURL=03-promises.f8827801.js.map
