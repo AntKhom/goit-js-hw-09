b7f7884c267432c3ab3c577bb3ad6d55562a698b
AntKhom <antonhomenko1988@gmail.com>
2023-08-30 12:43:27 -0400

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,28 @@
+const form = document.querySelector('.form');
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve('ok');
+      } else {
+        reject('no');
+      };
+    }, delay);
+  });
+  promise.then(result => {
+    alert(result);
+  });
+  promise.cutch(result => {
+    alert(result);
+  });
+};
+   
+
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(e);
+  createPromise(1,1);
+})
