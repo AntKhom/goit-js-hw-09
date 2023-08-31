ef81d88c9619e0ab0cf1a8eaee2c1af0c82e2a37
AntKhom <antonhomenko1988@gmail.com>
2023-08-31 13:51:39 -0400

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,21 @@
-const form = document.querySelector('.form');
+
+const refs = {
+  form: document.querySelector('.form'),
+  inputDelay: document.querySelector('input[name=delay]'),
+  inputStep: document.querySelector('input[name=step]'),
+  inputAmount: document.querySelector('input[name=amount]'),
+}
+const position = 0;
+refs.inputDelay.setAttribute('step','100');
+refs.inputStep.setAttribute('step','100');
+
+// let delay = inputDelay.value;
+// const step = inputStep.value;
+// const amount = inputAmount.value;
+
+
 function createPromise(position, delay) {
-  const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
@@ -10,18 +25,38 @@ function createPromise(position, delay) {
       };
     }, delay);
   });
-  return promise;
 };
   
-const ok = ({ position, delay }) => {
+const resolvePromise = ({ position, delay }) => {
   console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
 };
-const no = ({ position, delay }) => {
+const rejectPromise = ({ position, delay }) => {
     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  };
+};
    
-form.addEventListener('submit', (e) => {
+refs.form.addEventListener('submit', (e) => {
   e.preventDefault();
-//  console.log(e);
-  createPromise(2,1500).then(ok).catch(no);
+  const values = { 
+    delay: +e.target.delay.value,
+    step: +e.target.step.value,
+    amount: +e.target.amount.value,
+  };
+  makeArrPromise(values);
+  //console.log(makeArrPromise(values));
+
+  console.log(values);
+  
+  //createPromise(2,1500).then(resolvePromise).catch(rejectPromise);
 })
+
+const makeArrPromise = ({ delay, step, amount }) => {
+  const promises = [];
+  for (let i = 0; i < amount; i+=1) {
+      //promises[i] = createPromise(5, 1000);
+      promises[i] = createPromise(i, delay);
+      delay += step;
+  };
+
+  console.log(promises);
+  Promise.all(promises).then(resolvePromise).catch(rejectPromise);
+};
